18365fc551fdda569b9010cbc5fe7e8b2e1f41af
Peter <peter86tsai@gmail.com>
2025-07-09 21:47:23 +0800

    calculate.js更新

--- dist/site_20250709/assets/js/calculate.js
+++ dist/site_20250709/assets/js/calculate.js
@@ -23,31 +23,61 @@ const calcRun = () => {
     // }
 
     // 2. 判斷使用者是否輸入數字
-    if (value1 && value2 && op) {
-        let result;
-        // 3. 判斷 op
-        if (op == "+") {
-            result = value1 + value2;
-        }
-
-        if (op == "-") {
-            result = value1 - value2;
-        }
-
-        if (op == "*") {
-            result = value1 * value2;
-        }
-
-        if (op == "/") {
-            result = value1 / value2;
-        }
-
-        // 4. 顯示結果到 UI 上
-        console.log(result);
-        resultTitle.innerHTML = "計算結果:";
-        resultNum.innerHTML = result;
-    } else {
+    if (!value1 || !value2 || !op) {
         alert("請輸入數字");
+        return;
     }
+
+    let result;
+    // 3. 判斷 op
+    if (op == "+") {
+        result = value1 + value2;
+    }
+
+    if (op == "-") {
+        result = value1 - value2;
+    }
+
+    if (op == "*") {
+        result = value1 * value2;
+    }
+
+    if (op == "/") {
+        result = value1 / value2;
+    }
+
+    // hashmap作法
+    // 用物件來運行程式，最後抱進一個變數裡。
+    // op要記得先做錯誤判斷，或用try...catch...，避免變數噴錯。
+    // let ops = {
+    //     "+"(a, b) {
+    //         return a + b;
+    //     },
+    //     "-"() {},
+    // };
+    // let result = ops[op](value1, value2);
+
+    // switch作法
+    // switch (op) {
+    //     case "-":
+    //         result = value1 - value2;
+    //         break;
+
+    //     case "*":
+    //         result = value1 * value2;
+    //         break;
+
+    //     case "/":
+    //         result = value1 / value2;
+    //         break;
+
+    //     default:
+    //         result = value1 + value2;
+    // }
+
+    // 4. 顯示結果到 UI 上
+    console.log(result);
+    resultTitle.innerHTML = "計算結果:";
+    resultNum.innerHTML = result;
 };
 calcBtn.addEventListener("click", calcRun);
